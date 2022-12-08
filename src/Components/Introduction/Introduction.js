7244805b4269717b071bb451351b0f20a0b25b59
Gautham Vanjre <vanjregautham@gmail.com>
2022-12-08 17:21:33 +0530

    completed the validation part

--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -4,6 +4,22 @@ import illustration1 from "../../images/illustration-1.svg";
 import "./Introduction.css";
 
 const Introduction = () => {
+  const [userInput, setUserInput] = useState("");
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  const handleInput = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (userInput.trim().length !== 0) {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
+    }
+  };
+
   return (
     <section className="intro">
       <div className="intro-content">
@@ -13,9 +29,21 @@ const Introduction = () => {
           them wherever you need, share and collaborate with friends, family,
           and co-workers.
         </p>
-        <div className="input-wrapper">
-          <input type="text" placeholder="Enter your email" />
-          <button>Get started</button>
+        {/* input-wrapper */}
+        <div className={isEmpty ? "input-wrapper empty" : "input-wrapper"}>
+          <div className="empty-input">
+            <input
+              type="text"
+              onChange={handleInput}
+              value={userInput}
+              placeholder="Enter your email"
+              autoComplete="off"
+            />
+            <div className={isEmpty ? "empty-warning" : "empty-none"}>
+              Please check your email
+            </div>
+          </div>
+          <button onClick={handleClick}>Get started</button>
         </div>
       </div>
       <div className="intro-img-wrapper">
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 
 const Signup = () => {
+  const [userInput, setUserInput] = useState("");
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  const handleInput = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (userInput.trim().length !== 0) {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
+    }
+  };
   return (
     <section className="signup">
       <div className="signup-content">
@@ -12,9 +27,21 @@ const Signup = () => {
           be happy to help you.
         </p>
       </div>
-      <div className="signup-handler">
-        <input type="email" placeholder="enter you email" />
-        <button>Get Started For Free</button>
+      {/* signup-handler */}
+      <div className={isEmpty ? "signup-handler empty" : "signup-handler"}>
+        <div className="empty-input">
+          <input
+            type="email"
+            placeholder="enter you email"
+            onChange={handleInput}
+            value={userInput}
+            autoComplete="off"
+          />
+          <div className={isEmpty ? "empty-warning-msg" : "empty-none"}>
+            Please enter correct email
+          </div>
+        </div>
+        <button onClick={handleClick}>Get Started For Free</button>
       </div>
     </section>
   );
